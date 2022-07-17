897d57641be19ed5ed8a2e35039886326c0d2712
czo17437 <44199284+czo17437@users.noreply.github.com>
2022-07-17 18:27:45 +0200

    LvlDesignGerade

--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -22,7 +22,7 @@ public class Road : MonoBehaviour {
 		transform.position = ancor;
 		transform.forward = -normal;
 
-		if (presetContainer.transform.childCount > 1) {
+		if (presetContainer.transform.childCount > 0) {
 			int rand = Random.Range(0, presetContainer.transform.childCount - 1);
 			presetContainer.transform.GetChild(rand).gameObject.SetActive(true);
 		}
